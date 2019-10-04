3242b735e6df709478ad7c4bb5d91946eb6d7f1b
Andre Carbajal Vargas <40127869+andre-alejandro@users.noreply.github.com>
2019-10-04 09:57:07 -0500

    variable saturno y sol

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var usuario = prompt("Cual es tu peso?"); //lee la cadena
-        var planeta = parseInt( prompt("Elije el planeta:\n-1 es marte \n-2 es jupiter \n-3 es luna"));//pront es para leer una variable . \alt 92 \n sirve para hacer un salto de linea 
+        var usuario = prompt("Cual es tu peso?"); //lee la cadena
+        var planeta = parseInt( prompt("Elije el planeta:\n-1 es marte \n-2 es jupiter \n-3 es luna \n-4 es el sol \n-5 es saturno"));//pront es para leer una variable . \alt 92 \n sirve para hacer un salto de linea 
         var peso = parseFloat(usuario);//parseInt es para convertir la variable en un real
         var g_tierra = 9.8;
         var g_marte = 3.7;
         var g_jupiter = 24.8;
         var g_luna = 1.62;
+        var g_sol = 274
+        var g_saturno = 10.4
         var peso_final;
         var nombre ;
         if (planeta == 1)
@@ -21,6 +23,16 @@ var usuario = prompt("Cual es tu peso?"); //lee la cadena
         {
             peso_final = peso *  g_luna / g_tierra;
             nombre = "Luna";
+        }
+        else if (planeta == 4)
+        {
+            peso_final = peso *  g_sol / g_tierra;
+            nombre = "El sol";
+        }
+        else if (planeta == 5)
+        {
+            peso_final = peso *  g_saturno / g_tierra;
+            nombre = "Saturno";
         }
         else
             {
